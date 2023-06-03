60e046eacf8ce4c39a1a670c93549c5ddfc41ea0
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2023-06-02 21:22:24 -0300

    Implement playRound()

--- script.js
+++ script.js
@@ -3,3 +3,8 @@
 function getComputerChoice() {
     return Math.trunc(Math.random() * 3);
 }
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = (computerSelection + (1 - playerSelection) + 3) % 3;
+    return 1 - computerSelection;
+}
